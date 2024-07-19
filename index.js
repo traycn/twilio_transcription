4ccd90d44ad32c4fdcbc1561a41681566a90d8da
T <16641980+traycn@users.noreply.github.com>
2024-07-19 13:50:04 -0700

    Init commit. Create WebSocket server instance.

---
+++ index.js
@@ -0,0 +1,18 @@
+const WebSocket = require("ws");
+const express = require("express");
+
+const app = express();
+const server = require("http").createServer(app);
+const wss = new WebSocket.Server({ server });
+
+//Handle Web Socket Connection
+wss.on("connection", function connection(ws) {
+  console.log("New Connection Initiated");
+})
+
+//Handle HTTP Request
+app.get("/", (req, res) => res.send("Hello World"));
+
+//Start server
+console.log("Listening at Port 8080");
+server.listen(8080);
