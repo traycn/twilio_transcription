a73b64f5357d993e3ffc600d9346d988da14607a
T <16641980+traycn@users.noreply.github.com>
2024-07-19 17:35:59 -0700

    Define call actions.

--- index.js
+++ index.js
@@ -7,11 +7,47 @@ const wss = new WebSocket.Server({ server });
 
 //Handle Web Socket Connection
 wss.on("connection", function connection(ws) {
+  
+  ws.on("message", function incoming(message) {
+    const msg = JSON.parse(message);
+    switch (msg.event) {
+      case "connected":
+        console.log(`A new call has connected.`);
+        break;
+      case "start":
+        console.log(`Starting Media Stream ${msg.streamSid}`);
+        break;
+      case "media":
+        console.log(`Receiving Audio...`);
+        break;
+      case "stop":
+        console.log(`Call Has Ended`);
+        break;
+    }
+  });
+
   console.log("New Connection Initiated");
 })
 
 //Handle HTTP Request
 app.get("/", (req, res) => res.send("Hello World"));
+
+app.post("/", async (req, res) => {
+
+  res.set("Content-Type", "text/xml");
+  res.send(
+    `<Response>
+      <Start>
+        <Stream url='wss://${req.headers.host}' />
+      </Start>
+      <Say>
+        Start speaking to see your audio transcribed in the console.
+      </Say>
+      <Pause length='30' />
+    </Response>`
+  );
+
+});
 
 //Start server
 console.log("Listening at Port 8080");
